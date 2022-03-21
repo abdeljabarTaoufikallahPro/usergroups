0ed962fc461e8efd60597b535332c0a75a960e42
abdeljabar <taoufikallah@gmail.com>
2022-03-21 11:19:42 +0100

    add nelmio cors to fix cors issue

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
